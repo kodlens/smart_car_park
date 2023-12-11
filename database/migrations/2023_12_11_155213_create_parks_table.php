541c414c133c262b5628971ff037c801449d01b2
etienne <etiennewayne@gmail.com>
2023-12-11 16:26:25 +0800

    update repo

---
+++ database/migrations/2023_12_11_155213_create_parks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parks', function (Blueprint $table) {
+            $table->id('park_id');
+            $table->string('name', 50)->nullable();
+            $table->string('device_mac', 100)->nullable();
+            $table->string('device_ip', 100)->nullable();
+            $table->tinyInteger('is_occupied')->nullable()
+                ->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parks');
+    }
+}
