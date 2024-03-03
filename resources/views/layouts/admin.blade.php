f19643a714af97e50182817e7dfc0cc3dbc2e6fd
etiennewayne <etiennewayne@gmail.com>
2024-03-03 13:29:57 +0800

    update layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,8 +18,8 @@
     {{--    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="/css/app.css" rel="stylesheet">
     </style>
 </head>
 
