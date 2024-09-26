9d70270fb8c533960348f7d4287b3f97bb770195
etienne <etiennewayne@gmail.com>
2024-09-27 03:53:00 +0800

    add seeder for sales report test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ParkSeeder::class,
-            ParkPriceSeeder::class
+            ParkPriceSeeder::class,
+            ParkSaleSeeder::class
         ]);
     }
 }
---
+++ database/seeders/ParkSaleSeeder.php
@@ -0,0 +1,488 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ParkSaleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-20',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-25',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-26',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-04-28',
+            ],
+
+            //============================================
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-05-20',
+            ],
+            
+            //============================================
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-02',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-04',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-05',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-08',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-20',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-25',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-26',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-06-28',
+            ],
+
+            //===========================================
+
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-02',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-04',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-08',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-20',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-25',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-26',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-07-28',
+            ],
+            //======================================
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-02',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-04',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-05',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-08',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-20',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-25',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-26',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-08-28',
+            ],
+            //=======================================
+
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-01',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-02',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-04',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-05',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-08',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-11',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-12',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-15',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-16',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-18',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-19',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-20',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-25',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-26',
+            ],
+            [
+                'remarks' => 'reservation fee',
+                'price' => 20,
+                'transaction_date' => '2024-09-28',
+            ],
+
+          
+        ];
+
+        \App\Models\ParkSale::insertOrIgnore($data);
+    }
+}
