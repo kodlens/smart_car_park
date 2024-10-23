86592620b85cb709ca4b805ee5a6f91bc1179200
etienne <etiennewayne@gmail.com>
2024-10-23 23:16:29 +0800

    refactor semaphore module

--- app/Console/Commands/SMSNotif.php
+++ app/Console/Commands/SMSNotif.php
@@ -48,20 +48,16 @@ class SMSNotif extends Command
     public function handle()
     {
         if(env('SEMAPHORE_SERVICE') == 1){
-            $this->semaphoreSMS();
+            $this->sendSemaphoreSMS();
         }
 
-        // if(env('SEMYSMS_SERVICE') == 1){
-        //     $this->semySMS();
-        // }
-
         if(env('EMAIL_NOTIF') == 1){
-            $this->emailNotif();
+            $this->sendEmailNotif();
         }
         
     }
 
-    public function emailNotif(){
+    public function sendEmailNotif(){
         
         $settings = \DB::table('settings')->get();
         $notifBeforeEntrance = $settings->firstWhere('setting_name', 'notif_before_entrance');
@@ -126,239 +122,73 @@ class SMSNotif extends Command
 
             } //end foreach
         }
-
     }
 
-    public function semaphoreSMS(){
-
-        $beforeEnter = Carbon::now()->addMinutes(10);
-        $errorEnter = Carbon::now()->addMinutes(11);
-        $enter = ParkReservation::whereBetween('start_time', [$beforeEnter, $errorEnter])->with('user')->with('park')->get();
-
-        $beforeExit = Carbon::now()->addMinutes(30);
-        $errorExit = Carbon::now()->addMinutes(31);
-        $exit = ParkReservation::whereBetween('end_time', [$beforeExit, $errorExit])->with('user')->with('park')->get();
-
+    public function sendSMS($mobile, $msg, $remarks, $recipientName){
         $apiKey =  env('SEND_SMS');
-
-        if ($enter) {
-            foreach ($enter as $user) {
-                $ch = curl_init();
-                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': \nYour ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will start 10 minutes from now. Thank You!';
-                $parameters = array(
-                    'apikey' => $apiKey,
-                    'number' => $user->user->contact_no,
-                    'message' =>  $msg,
-                    'sendername' => 'CARSYS'
-                );
-                // curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/priority');
-                curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
-                curl_setopt($ch, CURLOPT_POST, 1);
-
-                //Send the parameters set above with the request
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-
-                // Receive response from server
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $output = curl_exec($ch);
-                curl_close($ch);
-
-                SmsLog::create([
-                    'contact_no' => $user->user->contact_no,
-                    'remarks' => 'ENTRANCE/SMS',
-                    'recipient' => $user->user->lname . ', ' . $user->user->fname,
-                    'msg' => $msg,
-                ]);
-
-            } //end foreach
-        }
-
-        
-        if ($exit) {
-            foreach ($exit as $user) {
-                $ch = curl_init();
-                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': Your ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will end 30 minutes from now. Thank You!';
-
-                $parameters = array(
-                    'apikey' => $apiKey,
-                    'number' => $user->user->contact_no,
-                    'message' => $msg,
-                    'sendername' => 'CARSYS'
-                );
-                return $parameters;
-                //curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/priority');
-                curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
-                curl_setopt($ch, CURLOPT_POST, 1);
-
-                //Send the parameters set above with the request
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-
-                // Receive response from server
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $output = curl_exec($ch);
-                curl_close($ch);
-
-                SmsLog::create([
-                    'contact_no' => $user->user->contact_no,
-                    'remarks' => 'EXIT/SMS',
-                    'recipient' => $user->user->lname . ', ' . $user->user->fname,
-                    'msg' => $msg,
-                ]);
-            } //end foreach
-        }
-    }
-
-    public function semySMS(){
-
-        $beforeEnter = Carbon::now()->addMinutes(10);
-        $errorEnter = Carbon::now()->addMinutes(11);
-        $enter = ParkReservation::whereBetween('start_time', [$beforeEnter, $errorEnter])->with('user')->with('park')->get();
-
-        $beforeExit = Carbon::now()->addMinutes(30);
-        $errorExit = Carbon::now()->addMinutes(31);
-        $exit = ParkReservation::whereBetween('end_time', [$beforeExit, $errorExit])->with('user')->with('park')->get();
-
-        $apiKey =  env('SEMY_SMS_API_TOKEN');
-        $url =  env('SEMY_SMS_URL');
-        $device = '1';  //  Device code
-
-        
-
-        if ($enter) {
-            foreach ($enter as $user) {
-                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': Your ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will start 10 minutes from now. Thank You!';
-                $contact = $user->user->contact_no;
-
-                //logic for gateway here...
-               
-                $data = array(
-                    "phone" => $contact,
-                    "msg" => $msg,
-                    "device" => $device,
-                    "token" => $apiKey
-                );
-               
-                   $curl = curl_init($url);
-                   curl_setopt($curl, CURLOPT_POST, true);
-                   curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-                   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                   curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);     
-                   $output = curl_exec($curl);
-                   curl_close($curl);
-
-                SmsLog::create([
-                    'contact_no' => $contact,
-                    'remarks' => 'ENTRANCE',
-                    'recipient' => $user->user->lname . ', ' . $user->user->fname,
-                    'msg' => $msg,
-
-                ]);
-
-            } //end foreach
-        }
-
-        
-        if ($exit) {
-
-            foreach ($exit as $user) {
-                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': \nYour ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will end 30 minutes from now. Thank You!';
-                $contact = $user->user->contact_no;
-                
-                //logic for gateway here...
-               
-
-                $data = array(
-                    "phone" => $contact,
-                    "msg" => $msg,
-                    "device" => $device,
-                    "token" => $apiKey
-                );
-               
-                   $curl = curl_init($url);
-                   curl_setopt($curl, CURLOPT_POST, true);
-                   curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-                   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-                   curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);     
-                   $output = curl_exec($curl);
-                   curl_close($curl);
-
-                SmsLog::create([
-                    'contact_no' => $contact,
-                    'remarks' => 'ENTRANCE',
-                    'recipient' => $user->user->lname . ', ' . $user->user->fname,
-                    'msg' => $msg,
-
-                ]);
-
-                SmsLog::create([
-                    'contact_no' => $user->user->contact_no,
-                    'remarks' => 'EXIT',
-                    'recipient' => $user->user->lname . ', ' . $user->user->fname,
-                    'msg' => $msg,
-
-                ]);
-            } //end foreach
-        }
-    }
-
-    /**THIS IS FOR TESTING SEMAPHORE */
-    // public function mySMS(){
-    //     $apiKey =  env('SEND_SMS');
-    //     $ch = curl_init();
-    //     $parameters = array(
-    //         'apikey' => $apiKey,
-    //         'number' => '09706102876',
-    //         'message' => 'SMS Test',
-    //         'sendername' => 'SEMAPHORE'
-    //     );
-    //     curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
-    //     curl_setopt($ch, CURLOPT_POST, 1);
-
-    //     //Send the parameters set above with the request
-    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
-
-    //     // Receive response from server
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //     $output = curl_exec($ch);
-    //     curl_close($ch);
-    // }
-
-    public function mySemySMS(){
-
-        
-        $apiKey =  env('SEMY_SMS_API_TOKEN');
-        $url =  env('SEMY_SMS_URL');
-        $device = '1';  //  Device code
-       
-        $msg = 'This is automated SMS using SEMYSMS!';
-        $contact = '09683013603';
-                
-        //logic for gateway here...    
-
-        $data = array(
-            "phone" => $contact,
-            "msg" => $msg,
-            "device" => $device,
-            "token" => $apiKey
+        $parameters = array(
+            'apikey' => $apiKey,
+            'number' => $mobile,
+            'message' =>  $msg,
+            'sendername' => 'CARSYS'
         );
-        
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);     
-        $output = curl_exec($curl);
-        curl_close($curl);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://semaphore.co/api/v4/messages');
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        //Send the parameters set above with the request
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+
+        // Receive response from server
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+        curl_close($ch);
 
         SmsLog::create([
-            'contact_no' => $contact,
-            'remarks' => 'TEST',
-            'recipient' => 'AMPARADO',
-            'msg' => 'TEST OF SEMYSMS',
+            'contact_no' => $mobile,
+            'remarks' => $remarks,
+            'recipient' => $recipientName,
+            'msg' => $msg,
         ]);
-
     }
+
+    public function sendSemaphoreSMS(){
+
+        $beforeEnter = Carbon::now()->addMinutes(10);
+        $errorEnter = Carbon::now()->addMinutes(11);
+        $enter = ParkReservation::whereBetween('start_time', [$beforeEnter, $errorEnter])->with('user')->with('park')->get();
+
+        $beforeExit = Carbon::now()->addMinutes(30);
+        $errorExit = Carbon::now()->addMinutes(31);
+        $exit = ParkReservation::whereBetween('end_time', [$beforeExit, $errorExit])->with('user')->with('park')->get();
+
+
+        if ($enter) {
+            foreach ($enter as $user) {
+
+                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': Your ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will start 10 minutes from now. Thank You!';
+                $contactNo = $user->user->contact_no;
+                $remarks = 'ENTRANCE/SMS';
+                $recipientName = $user->user->lname . ', ' . $user->user->fname;
+
+                $this->sendSMS($contactNo, $msg, $remarks, $recipientName);
+
+            } //end foreach
+        }
+        
+        if ($exit) {
+            foreach ($exit as $user) {
+
+                $msg = 'Reminders Mr/Mrs. ' . $user->user->lname . ': Your ' . $user->hour . ' hr(s) Park Reservation at ' . $user->park->name . ' will end 30 minutes from now. Thank You!';
+                $contactNo = $user->user->contact_no;
+                $remarks = 'EXIT/SMS';
+                $recipientName = $user->user->lname . ', ' . $user->user->fname;
+
+                $this->sendSMS($contactNo, $msg, $remarks, $recipientName);
+
+            } //end foreach
+        }
+    }
+
+    
 }
